044730efbc7686674346006d4e09d95beed1df9d
William Sullivan <sullivanwp95@gmail.com>
2022-05-19 17:21:03 -0400

    defining and implementing eat_lunch function

--- functions_practice.py
+++ functions_practice.py
@@ -7,3 +7,17 @@ def pack(item_1, item_2, item_3):
     return [item_1, item_2, item_3]
 
 print(pack("swimsuit", "toothbrush", "cards"))
+
+def eat_lunch(list):
+    if len(list) == 0:
+        print("My lunchbox is empty!")
+    else:
+        for i in range(len(list)):
+            if i == 0:
+                print(f"First I eat {list[i]}")
+            else:
+                print(f"Next I eat {list[i]}")
+
+eat_lunch([])
+eat_lunch(["sushi"])
+eat_lunch(["sandwich","chips","pickle"])
